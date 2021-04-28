ce35ce3d6cd672b49003541c654556db19b28ac8
pinkflamingo108 <64995424+pinkflamingo108@users.noreply.github.com>
2021-04-28 11:27:30 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,43 +1,54 @@
-// Create Dino Constructor
+// Submit Values
+// All the data will go to this array
 
-function Dino(species, weight, height, diet, where, when, fact) {
- this.species = species;
- this.weight = weight;
- this.height = height;
- this.diet = diet;
- this.where = where;
- this.when = when;
- this.fact = fact;
- this.image = "/images" + species.toLowerCase() + ".png";
-}
+const arrayOfDino = [];
+
+//This prints what is in the array
+console.log(arrayOfDino);
+
 // Create Dino Objects
 
 fetch("dino.json")
  .then((response) => response.json())
  .then((data) => {
   let dinoData = data.Dinos;
-  return getDinos(dinoData);
+  dinoData.map((e) => {
+   let dinoValues = Object.values(e);
+   let dinoConstructor = new Dino(...dinoValues);
+   arrayOfDino.push(dinoConstructor);
+  });
  })
  .catch((err) => {
   console.log("Error");
  });
 
-function getDinos(data) {
- data.map((info) => {
-  console.log(info);
- });
+// Create Dino Constructor
+
+class Dino {
+ constructor(species, weight, height, diet, where, when, fact) {
+  this.species = species;
+  this.weight = weight;
+  this.height = height;
+  this.diet = diet;
+  this.where = where;
+  this.when = when;
+  this.fact = fact;
+ }
+
+ weight() {
+  console.log(this.species, "has arrived to the meeting");
+ }
 }
+
 // Create Human Object
 
-// function Human(name, feet, inches, weight, diet) {
-//  this.name = document.getElementById("name").value;
-//  this.feet = document.getElementById("feet").value;
-//  this.inches = document.getElementById("inches").value;
-//  this.weight = document.getElementById("weight").value;
-//  this.diet = document.getElementById("diet").value;
-// }
-
-// Use IIFE to get human data from form
+function Human() {
+ this.name = document.getElementById("name").value;
+ this.feet = document.getElementById("feet").value;
+ this.inches = document.getElementById("inches").value;
+ this.weight = document.getElementById("weight").value;
+ this.diet = document.getElementById("diet").value;
+}
 
 // Create Dino Compare Method 1
 // NOTE: Weight in JSON file is in lbs, height in inches.
@@ -53,5 +64,18 @@ function getDinos(data) {
 // Add tiles to DOM
 
 // Remove form from screen
+// Submit Button
+
+const submitBtn = document.getElementById("btn");
+const form = document.getElementById("dino-compare");
+
+submitBtn.addEventListener("click", () => {
+ form.style.display = "none";
+ // Use IIFE to get human data from form
+ (function name() {
+  let human = new Human();
+  arrayOfDino.push(human);
+ })();
+});
 
 // On button click, prepare and display infographic
