b5ff680539d05ee69b72256e0551864a7c6dee3a
pinkflamingo108 <64995424+pinkflamingo108@users.noreply.github.com>
2021-05-10 22:50:26 -0500

    Update app.js

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ Dino.prototype.dinoHeight = function () {
  let dinoH = parseInt(this.height);
  if (dinoH > humanTotal) {
   return `Dino is taller by ${dinoH - humanTotal} inches`;
- } else if (dinoH < humanW) {
+ } else if (dinoH < humanTotal) {
   return `The human is bigger by ${humanTotal - dinoH}`;
  } else {
   return ` Oh my god! Both of you weight the same!`;
@@ -59,9 +59,9 @@ Dino.prototype.dinoWeight = function () {
  if (dinoW > humanW) {
   return `The Dinosaur is heavier by ${dinoW - humanW}`;
  } else if (humanW > dinoW) {
-  return `Incredible! The human is ${humanW - dinoW} than the Dino`;
+  return `Incredible! The human is ${humanW - dinoW} lbs than the Dino`;
  } else {
-  retun`Both weight the same amount`;
+  return `Both weight the same amount`;
  }
 };
 
@@ -70,20 +70,36 @@ Dino.prototype.dinoWeight = function () {
 Dino.prototype.dinoDiet = function () {
  let human = new Human();
  let humanD = human["diet"];
+ let humanDL = humanD.toLowerCase();
  let dinoD = this.diet;
+ let dinoDL = this.diet.toLowerCase();
 
- if (humanD === dinoD) {
-  console.log(`Both the dinosaur and the human have the same diet!`);
- } else {
+ if (humanDL == dinoDL) {
   console.log(
-   `They eat different food. The human eats ${humanD} and the Dino eats ${dinoD}`
+   `Both the dinosaur and the human have the same diet! They both eat ${humanDL}`
+  );
+ } else if (humanDL != dinoDL) {
+  console.log(
+   `They eat different food. The human eats ${humanDL} and the Dino eats ${dinoDL}`
   );
  }
 };
 
-// This is a test prototype since it doesn't require the human prototype
-Dino.prototype.testCase = function () {
- console.log(`${this.weight} is my weight`);
+// Create Dino Compare Method 4
+
+Dino.prototype.facts = function () {
+ return `${this.fact}`;
+};
+
+Dino.prototype.randomFact = function () {
+ let arrayM = [
+  this.fact(),
+  this.dinoDiet(),
+  this.dinoWeight(),
+  this.dinoHeight(),
+ ];
+ let math = parseInt(Math.random() * arrayM.length);
+ console.log(arrayM[math]);
 };
 
 // Create Human Object
@@ -95,14 +111,6 @@ function Human() {
  this.weight = document.getElementById("weight").value;
  this.diet = document.getElementById("diet").value;
 }
-
-// This was only to test my human prototype
-Human.prototype.humanTesting = function () {
- console.log(this.diet);
- console.log(this.name);
-};
-
-// Generate Tiles for each Dino in Array
 
 // Add tiles to DOM
 
@@ -118,10 +126,11 @@ submitBtn.addEventListener("click", () => {
  // Use IIFE to get human data from form
  (function name() {
   let human = new Human();
-  human.humanTesting();
+
   arrayOfHuman.push(human);
-  arrayOfDino.forEach((e) => {
-   console.log(e);
+  arrayOfDino.forEach((arrayInfo) => {
+   //Here im trying to call it without properties
+   console.log(arrayInfo.randomFact());
   });
  })();
 });
